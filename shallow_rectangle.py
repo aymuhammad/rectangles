c24752e2a6ae9d1d951bcd533ee43ffb14516ffe
Auwal Yahaya <auwalyah@yahoo.com>
2020-03-06 11:00:26 +0100

    Create shallow_rectangle.py

---
+++ shallow_rectangle.py
@@ -0,0 +1,14 @@
+def rectangle_box(n, m):
+	for i in range (1, n + 1):
+		for j in range(1, m + 1):
+			if ( i == 1 or i == n or j == 1 or j == m):
+				print('*', end = '')
+			else:
+				print(' ', end='')
+		print()
+
+
+
+rows = 7
+columns = 20
+rectangle_box(rows, columns)
